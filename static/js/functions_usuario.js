53c682da581c2435dadc121e9951a3961ee41816
Angel Dev <57205952+angeldevtech@users.noreply.github.com>
2021-11-19 01:56:13 -0500

    Update delete user

--- static/js/functions_usuario.js
+++ static/js/functions_usuario.js
@@ -90,6 +90,10 @@ function edit_usuario(){
 	});
 }
 
+function eli_usuario(){
+	
+}
+
 function eli_usuario_tempo(){
 	$("#tab_usuarios").on("click",".btn-eli-usu",function(){
 		var id_usu = this.getAttribute("rl");
