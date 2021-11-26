3aa6ba6680d446bf90f6a112399a872eee9610cb
Anr2112 <anderson.nilupu.r@uni.pe>
2021-11-26 14:30:53 -0500

    Lista de roles

--- app.py
+++ app.py
@@ -496,15 +496,15 @@ def list_roles():
 	data_final = list()
 	for i in range(len(data)):
 		if (i+1) == len(data):
-			data_permiso.append(data[i][2])
+			data_permiso.append('<p class="text-center">'+data[i][2]+'</p>')
 			data_nexada.append(data[i][0])
 			data_nexada.append(data[i][1])
 			data_nexada.append([data_permiso])
 			data_final.append(data_nexada)
 		elif data[i][0] == data[i+1][0]:
-			data_permiso.append(data[i][2])
+			data_permiso.append('<p class="text-center">'+data[i][2]+'</p>')
 		else :
-			data_permiso.append(data[i][2])
+			data_permiso.append('<p class="text-center">'+data[i][2]+'</p>')
 			data_nexada.append(data[i][0])
 			data_nexada.append(data[i][1])
 			data_nexada.append([data_permiso])
