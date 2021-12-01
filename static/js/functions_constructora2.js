9974ecbe3841d44067c522d1c58a1cb26e6ebd07
Kuroh2502 <jtellovallejo@gmail.com>
2021-11-30 21:06:44 -0500

    Corrección de funcionalidades en Construcción Pública

--- static/js/functions_constructora2.js
+++ static/js/functions_constructora2.js
@@ -23,14 +23,15 @@ function list_construc(){
 			"dataSrc": ""
 		},
 		"columns":[
-			{"data":"0"},
+			//{"data":"0"},
 			{"data":"1"},
 			{"data":"2"},
 			{"data":"3"},
 			{"data":"4"},
 			{"data":"5"},
 			{"data":"6"},
-			{"data":"7"}
+			{"data":"7"},
+			{"data":"8"}
 		],
 		"responsive": true,
 		"bDestroy": true,
